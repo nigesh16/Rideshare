2f1ee185399181a189e5adf98f111dd4ba53ce61
nigesh <nigeshnigesh214@gmail.com>
2025-10-13 21:26:25 +0530

    Update verification.js

--- backend/routes/driver/verification.js
+++ backend/routes/driver/verification.js
@@ -8,13 +8,11 @@ const router = express.Router();
 
 // Nodemailer transporter
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 // Email check & send OTP
